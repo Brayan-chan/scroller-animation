7c9ca3089d2ccc1d81c45c5d0cf4133c24ab9ae4
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-06-02 11:22:50 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Ejemplo de como utilizar GSAp
+// Ejemplo utilizando ScrollTrigger
+
+gsap.to(".a", {
+    x: 200, // 200 pixeles hacia la derecha
+    rotation: 360, // 360 grados de rotación
+    duration: 3, // Duración de 3 segundos de la animación
+})
