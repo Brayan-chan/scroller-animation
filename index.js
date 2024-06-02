3427df81f1b08b4eed1f41238bc68e6e7a22fc76
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-06-02 11:27:01 -0600

    animations wth scale and transformOrigin

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Ejemplo de como utilizar GSAp
 // Ejemplo utilizando ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".a", {
+gsap.to(".c", {
+    scrollTrigger: ".c", // Activar ScrollTrigger en el elemento .a
     x: 200, // 200 pixeles hacia la derecha
     rotation: 360, // 360 grados de rotación
     duration: 3, // Duración de 3 segundos de la animación
+    transformOrigin: "50% 50%", // Centrar la animación
+})
+
+
+gsap.to(".b", {
+    scrollTrigger: ".b", // Activar ScrollTrigger en el elemento .a
+    x: 200, // 200 pixeles hacia la derecha
+    rotation: 360, // 360 grados de rotación
+    duration: 3, // Duración de 3 segundos de la animación
+    transformOrigin: "50% 50%", // Centrar la animación
+    scale: 0.2, // Escalar la animación a 0.5
+    opacity: 0.5, // Opacidad de la animación a 0.5
 })
